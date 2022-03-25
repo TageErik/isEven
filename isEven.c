d4a1a2c550589436d9527eb29382181108f769e5
TageErik <79329572+TageErik@users.noreply.github.com>
2022-03-25 20:13:19 +0100

    Create isEven.c

---
+++ isEven.c
@@ -0,0 +1,11 @@
+int isEven(int num) {
+  char *a = "good morning";
+  char *b[(int)(log(num+!num)/log('c'>>3))];
+  int c=0;
+  for(int i=(int)(log(num+!num)/log('g'>>3)+1); i>0: i--) {
+    b[c]=&a[(int)(0-(int)(strlen(a))*((int)(num/pow(strlen(a),i))-num/pow(strlen(a),i)-0.0001))];
+    c++;
+   }
+   c=(*(c[b-1])+3+3*(c[b-1)-a))%34;
+   return !(c*(c*(c*(c*(c*(c-73)+1935)-23395)+131344)-306372)+196560);
+}
